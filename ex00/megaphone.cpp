2c9893a046aaf4ec5ed156f97d2fc60ac9a684cb
Gabriele Rinella <grinella@e3r3p4.42roma.it>
2024-05-15 15:56:49 +0200

    primo push

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main (int argc, char **argv)
+{
+	int i = 1;
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" <<std::endl;
+	else
+	{
+		std::string  stringa = argv[i];
+		for (size_t i = 0; i < stringa.size(); i++)
+		{
+			
+		}
+	}
+	return 0;
+}
