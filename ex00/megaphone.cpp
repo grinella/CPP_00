a1addee15dca64cb0c08db5080fd9466648d48b7
Gabriele Rinella <grinella@e3r3p4.42roma.it>
2024-05-16 13:43:33 +0200

    ex00 finito

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -2,16 +2,20 @@
 
 int main (int argc, char **argv)
 {
-	int i = 1;
 	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" <<std::endl;
 	else
 	{
-		std::string  stringa = argv[i];
-		for (size_t i = 0; i < stringa.size(); i++)
+		for (int i = 1; argv[i]; i++)
 		{
-			
+			std::string  stringa = argv[i];
+			for (size_t j = 0; j < stringa.size(); j++)
+			{
+				stringa[j]=toupper(stringa[j]);
+				std::cout << stringa[j];
+			}
 		}
+		std::cout <<std::endl;
 	}
 	return 0;
 }
